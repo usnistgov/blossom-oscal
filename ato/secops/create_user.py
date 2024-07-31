d18d56376f182116682c57048707fa8b93466ba2
selenaxiao-nist <selena.xiao@nist.gov>
2024-07-31 11:41:06 -0400

    Add username

--- ato/secops/create_user.py
+++ ato/secops/create_user.py
@@ -4,17 +4,19 @@ from datetime import datetime
 
 @click.command()
 @click.option('--user-name', required=True, help='Name of user to create')
+@click.option('--user-username', required=True, help='Name of user to create')
 @click.option('--user-email', required=True, help='Email of user to create')
 @click.option('--user-role', required=True, help='Role of user to create')
 @click.option('--location-uuid', help='UUID of physical location of user')
 @click.option('--org-member', help='UUID of organization that user is member of')
 @click.option('--issue-number', help='Issue number of user account request')
-def create_user(user_name, user_email, user_role, location_uuid, org_member, issue_number):
+def create_user(user_name, user_username, user_email, user_role, location_uuid, org_member, issue_number):
     """
     Creates a yaml file containing information about a new user
 
     Args:
         user_name (string): String containing name of new user
+        user_username (string): String containing username of new user
         user_email (string): String containing email of new user
         user_role (string): String containing role of new user
         location_uuid (string): String containing UUID of physical location of user
@@ -27,6 +29,7 @@ def create_user(user_name, user_email, user_role, location_uuid, org_member, iss
     "command" : "create-acl-user|create-cognito-user",
     "user" : {
         "name" : f"{user_name}",
+        "username" : f"{user_username}",
         "email-address":f"{user_email}",
         "role":f"{user_role}",
         "location-uuid":f"{location_uuid}",
